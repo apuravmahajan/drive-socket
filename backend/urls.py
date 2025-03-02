79b3472e9c6c74d8c941718c84fee37da3205fba
Apurav Mahajan <mahajanapurav991@gmail.com>
2025-03-02 22:07:10 +0530

    updated /

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,8 @@
+from oauth.views import GoogleLoginView
 from django.urls import path, include
 
 urlpatterns = [
+    path('/', GoogleLoginView, name="google_login"),
     path('auth/', include("oauth.urls")),
     path('drive/', include("drive_app.urls"))
 ]
